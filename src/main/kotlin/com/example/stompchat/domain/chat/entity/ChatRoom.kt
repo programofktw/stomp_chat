937d63503baa5df3674b453081a0496a755cf570
programofktw <thkim000317@naver.com>
2025-05-09 17:36:05 +0900

    ChatRoom 과 ChatText 엔티티 설계

--- src/main/kotlin/com/example/stompchat/domain/chat/entity/ChatRoom.kt
+++ src/main/kotlin/com/example/stompchat/domain/chat/entity/ChatRoom.kt
@@ -1,4 +1,17 @@
 package com.example.stompchat.domain.chat.entity
 
-class ChatRoom {
-}
+import jakarta.persistence.*
+import java.util.UUID
+
+@Entity
+data class ChatRoom(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long?,
+
+    @Column(name = "romeId")
+    var roomId : UUID,
+
+    @OneToMany(mappedBy = "chatRoom", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var chatText : MutableList<ChatText>
+)
